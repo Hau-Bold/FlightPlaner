f194c7d3e990ab43892cdd620c9495e56999d633
Philipp Felsecker <phil@containerize.de>
2021-01-05 00:51:06 +0100

    Refactor using spring to create a REST API * POST location already working * grep geo-coordinates from OpenStreetMap for the POSTed location * return jsonObject with the update location containing geo-coordinates

--- src/main/java/Routenplaner/Fonts.java
+++
@@ -1,9 +0,0 @@
-package Routenplaner;
-
-import java.awt.Font;
-
-public class Fonts {
-
-	public static final Font MainFont = new Font(Font.SERIF, Font.BOLD, 12);
-
-}
--- src/main/java/routePlanning/Contract/IOptimizationService.java
+++
@@ -1,9 +0,0 @@
-package routePlanning.Contract;
-
-import java.util.List;
-
-import routePlanning.Impl.GPSCoordinate;
-
-public interface IOptimizationService {
-	List<GPSCoordinate> compute(GPSCoordinate start, List<GPSCoordinate> targets);
-}
